327c301f991475d8d47971dc9177d3e5d8db2be0
Norman Huth <norman@huth.it>
2020-08-04 19:44:04 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // purge: [
+  //     './public/*.php',
+  //     './public/**/*.html',
+  // ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  // module: {
+  //   rules: [
+  //     {
+  //       use: [
+  //         {
+  //           loader: 'postcss-loader',
+  //           options: {
+  //             ident: 'postcss',
+  //             plugins: [
+  //               require('tailwindcss'),
+  //               require('autoprefixer'),
+  //             ],
+  //           },
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
+}
